78e406f009d317f92b284c72b4c1169cecfc9b28
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-27 11:47:50 +0500

    dict

--- first_item.py
+++ first_item.py
@@ -2,5 +2,7 @@ def first_item():
     """
     Given a dictionary, Return first item value.
     """
-    data = {'a': 1, 'b': 2}
-    return
+    data = {'a': 0, 'l': 8, 'b': 2}
+    for i in data:
+        return data[i]
+print(first_item())
