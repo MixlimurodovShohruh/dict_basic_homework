8bc5bedfa19828c6bffb71664dcf049eb81e87db
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-27 13:43:10 +0500

    dict

--- last_item.py
+++ last_item.py
@@ -3,4 +3,7 @@ def last_item():
     Given a dictionary, Return last item value.
     """
     data = {'a': 1, 'b': 2}
-    return
+    for i in data:
+        i=i
+    return data[i[-1]]
+print(last_item())
